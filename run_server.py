51851acd9521d4ede83ddc7aa071f6f9610039ec
seLain <selain@nature.ee.ncku.edu.tw>
2018-07-25 22:59:27 +0800

    add multiply & URL

--- run_server.py
+++ run_server.py
@@ -5,5 +5,13 @@ app = Flask(__name__)
 def index():
     return 'Index Page'
 
+@app.route('/<int:multiplier>/<int:multiplicand>')
+def multiply(multiplier, multiplicand):
+    # check if multiplier and multiplicand both int
+    if isinstance(multiplier, int) and isinstance(multiplicand, int):
+        return str(multiplier*multiplicand)
+    else:
+        return 'Only int is accepted'
+
 if __name__ == '__main__':
     app.run(debug=True)
